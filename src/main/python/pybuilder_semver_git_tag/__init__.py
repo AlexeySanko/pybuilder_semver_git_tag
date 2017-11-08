9bf40a9fb22af911d5c25c2bafd5331db3cb872a
AlexeySanko <asanko@braintch.com>
2017-11-08 11:22:37 +0300

    __version__: import module

--- src/main/python/pybuilder_semver_git_tag/__init__.py
+++ src/main/python/pybuilder_semver_git_tag/__init__.py
@@ -23,11 +23,11 @@ from pybuilder.plugins.python.core_plugin import DISTRIBUTION_PROPERTY
 from pybuilder.errors import BuildFailedException
 import semver
 
-from pybuilder_semver_git_tag.version import __version__
+from pybuilder_semver_git_tag import version
 
 
 __author__ = 'Alexey Sanko'
-__version__ = __version__
+__version__ = version.__version__
 
 use_plugin("python.core")
 
